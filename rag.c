7b829f447497b607e4c2d69130edd09bfdece8a6
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-07 18:49:08 -0400

    I made a helper :)

--- rag.c
+++ rag.c
@@ -35,17 +35,39 @@ void rag_print()
 
 void deadlock_detect()
 {
-    char* visited[NLOCK*NPROC];
-    int visitCounter = 0;
+    //char* visited[NLOCK*NPROC];
+    //int visitCounter = 0;
     for(int i = 0; i < (NLOCK+NPROC); i++) {
         for(int j = 0; j < (NLOCK+NPROC); j++) {
             if(rag[i][j] > 1) {
                 kprintf("DEADLOCK");
+                deadlockHelper();
+                break;
             }
             else if(rag[i][j] == 1) {
                 rag[i][j] += 1;
+                /**
                 visited[visitCounter] = i + "->" + j;
                 visitCounter += 1;
+                **/
+                i = j;
+                j = 0;
+            }
+        }
+    }
+}
+
+void deadlockHelper()
+{
+    for(int i = 0; i < (NLOCK+NPROC); i++)
+    {
+        for(int j = 0; j < (NLOCK+NPROC); j++)
+        {
+            if(rag[i][j] == 2) {
+                if(i > NLOCK)
+                    krpintf("pid=%d ", i-NLOCK); //proc
+                else
+                    kprintf("lockID=%d ", i);
                 i = j;
                 j = 0;
             }
