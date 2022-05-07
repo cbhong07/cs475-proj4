d7303a03bd49bb606f8b51d75ef55043dd9776c9
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-07 18:03:09 -0400

    First commit with the new non-XINU layout for RAG.C

---
+++ rag.c
@@ -0,0 +1,30 @@
+#define NLOCK 10
+#define NPROC 20
+
+#include <stdlib.h>
+
+void rag_request(int pid, int lockid)
+{
+
+}
+
+void rag_alloc(int pid, int lockid)
+{
+
+}
+
+void rag_dealloc(int pid, int lockid)
+{
+
+}
+
+void rag_print()
+{
+
+}
+
+void deadlock_detect()
+{
+
+}
+
